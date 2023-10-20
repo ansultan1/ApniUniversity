ef909575e7ebc064c762c2d5c31baf0cb59094e6
Ahmad N Sultan <ahmad.nabi@wanclouds.net>
2023-10-20 08:56:04 +0500

    "fixed a typo "

--- models/accounts.go
+++ models/accounts.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Accounts struct {
-	ID                   int         `json:"id" bson:"_id" structs:"id" db:"id"`
-	Type                 AccountType `json:"type" bson:"type" structs:"type" db:"type"`
-	TeacherPay           float64     `json:"teacherPay" bson:"teacher_pay" structs:"teacher_pay" db:"teacher_pay"`
-	StudentFee           float64     `json:"studentFee" bson:"student_fee" structs:"student_fee" db:"student_fee"`
-	IsDefault            bool        `json:"isDefault" bson:"is_default" structs:"is_default" db:"is_default"`
-	StudentOnInstallment bool        `json:"studentOnInstallment" bson:"student_on_installment" structs:"student_on_installment"`
-	InstallmentDetails   Installment `json:"installmentDetails" bson:"installment_details" structs:"installment_details" db:"installment_details"`
-	CreatedAt            time.Time   `json:"createdAt" bson:"created_at" structs:"created_at" db:"created_at"`
+	ID                 int         `json:"id" bson:"_id" structs:"id" db:"id"`
+	Type               AccountType `json:"type" bson:"type" structs:"type" db:"type"`
+	TeacherPay         float64     `json:"teacherPay" bson:"teacher_pay" structs:"teacher_pay" db:"teacher_pay"`
+	StudentFee         float64     `json:"studentFee" bson:"student_fee" structs:"student_fee" db:"student_fee"`
+	IsDefault          bool        `json:"isDefault" bson:"is_default" structs:"is_default" db:"is_default"`
+	OnInstallment      bool        `json:"OnInstallment" bson:"on_installment" structs:"on_installment" db:"on_installment"`
+	InstallmentDetails Installment `json:"installmentDetails" bson:"installment_details" structs:"installment_details" db:"installment_details"`
+	CreatedAt          time.Time   `json:"createdAt" bson:"created_at" structs:"created_at" db:"created_at"`
 }
 
 func (a *Accounts) Map() map[string]interface{} {
